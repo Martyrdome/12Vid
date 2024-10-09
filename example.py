6f3c13adf47ee3d64c56ae8d1511d19eb1f096a6
Zoid <108158093+TheZoidMaster@users.noreply.github.com>
2024-10-09 11:16:49 -0500

    added some stuff :trollface:

--- example.py
+++ example.py
@@ -4,7 +4,9 @@ from tw_vid import FileVideoFrameHandler
 import numpy as np
 
 
-def corrupt(frame):
+def corrupt(frame, _path):
+    # frame is a numpy array of shape (height, width, 3).
+    # path is the path to the frame file, only available in FileVideoFrameHandler corruption function.
     # Simple corruption function. Invert the color of each pixel.
     return np.invert(frame)
 
@@ -14,5 +16,5 @@ if __name__ == "__main__":
     handler.process("test.mp4", "test.avi")
     # Alternatively, you can call each step manually. This is mainly used with FileVideoFrameHandler, for using external corruption methods.
     # handler.extract_frames("test.mp4")
-    # handler.corrupt_frames()
+    # handler.corrupt_frames() or do.something.else()
     # handler.save("test.avi")
